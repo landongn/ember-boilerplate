cdc93f9e6db1f4471f2616d98ee6e0b79d992f1b
Landon Noss <landon.noss@ff0000.com>
2013-09-03 15:34:00 -0700

    boilerplate setup

---
+++ app/scripts/app.js
@@ -0,0 +1,8 @@
+var App = window.App = Ember.Application.create();
+
+/* Order and include as you please. */
+require('scripts/controllers/*');
+require('scripts/models/*');
+require('scripts/routes/*');
+require('scripts/views/*');
+require('scripts/router');
