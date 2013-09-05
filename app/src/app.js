9551896429ffe2a30a9716dc9b91a0332701c19d
Landon Noss <landon.noss@ff0000.com>
2013-09-05 16:49:30 -0700

    things

--- app/src/app.js
+++ app/src/app.js
@@ -1,5 +1,10 @@
 var App = window.App = Ember.Application.create();
 
 /* Order and include as you please. */
-require('modules/sampleModule/main');
+require('src/models/*');
+require('src/controllers/*');
+require('src/views/*');
+require('src/helpers/*');
+require('src/components/*');
+require("src/routes/*");
 require('src/router');
